05792ae532d23a39edfde02b88aeefe6fd1b528e
Madhav Dargar <77119265+madhavdargar@users.noreply.github.com>
2025-08-24 23:25:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["ai-transcript-summary.onrender.com"]
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
